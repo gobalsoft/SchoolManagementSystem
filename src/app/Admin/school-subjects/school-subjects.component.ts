69457f9bd8b5206cc75b15a9428da9b436430805
gobal <45533772+satheeshkumarsoft@users.noreply.github.com>
2020-09-28 22:15:51 +0530

    28/09/ changes

--- src/app/Admin/school-subjects/school-subjects.component.ts
+++ src/app/Admin/school-subjects/school-subjects.component.ts
@@ -12,9 +12,42 @@ import {Router} from "@angular/router";
 })
 export class SchoolSubjectsComponent implements OnInit {
 
+  SchoolSubForm: FormGroup;
+  SchoolSub:SchoolSubject[]=[];
+  submitted = false;
+
   constructor(private formBuilder: FormBuilder,private router: Router,private schoolService:SchoolService) { }
 
   ngOnInit(): void {
+
+    this.SchoolSubForm = this.formBuilder.group({
+
+      SubjectCode: ['', Validators.compose([ Validators.required, Validators.minLength(2), Validators.maxLength(5)])],
+      SubjectName: ['', Validators.required],
+      Category: ['',Validators.required],
+      SchoolID:['1'],
+  
+      
+    });
+     
+  
+
+
   }
+  get Subval() {
+    console.log(this.SchoolSubForm.controls);
+    return this.SchoolSubForm.controls;
+    }
+
+    onSubmit(){
+
+      debugger;
+           this.submitted = true;
+           if (this.SchoolSubForm.invalid) {
+           return;
+                        
+       }
+      
+         }
 
 }
