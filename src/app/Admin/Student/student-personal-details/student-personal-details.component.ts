d42b97bdab3db3564a0d251f2c56f1630f06c963
gobal <45533772+satheeshkumarsoft@users.noreply.github.com>
2020-11-24 21:50:04 +0530

    11/24/

--- src/app/Admin/Student/student-personal-details/student-personal-details.component.ts
+++ src/app/Admin/Student/student-personal-details/student-personal-details.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { SchoolService } from 'src/app/school.service';
+import { Student_Personal_Details } from 'src/app/student';
+import {
+  Religion,
+  Caste,
+  Category,
+  Disability,
+  Nationality,
+} from 'src/app/master';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-student-personal-details',
   templateUrl: './student-personal-details.component.html',
-  styleUrls: ['./student-personal-details.component.css']
+  styleUrls: ['./student-personal-details.component.css'],
 })
 export class StudentPersonalDetailsComponent implements OnInit {
-
-  constructor() { }
+  StudentPersonalForm: FormGroup;
+ StudentList :StudentPersonalDetailsComponent[] = [];
+  submitted = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private schoolService: SchoolService
+  ) {}
 
   ngOnInit(): void {
+    this.StudentPersonalForm = this.formBuilder.group({
+      FirstName: ['', Validators.required],
+      MiddleName:[],
+      LastName: ['', Validators.required],
+    });
   }
+  get StudentControl() {
+    console.log(this.StudentPersonalForm.controls);
+    return this.StudentPersonalForm.controls;
+  }
+  onSubmit() {
+    debugger;
+    console.log("submit");
+    this.submitted = true;
+    if (!this.StudentPersonalForm.invalid) {
+      console.log("ackabc");
+    }
 
+  }
 }
--- src/app/school.service.ts
+++ src/app/school.service.ts
@@ -1,125 +1,131 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from "@angular/common/http";
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 
-import {  throwError, Observable } from 'rxjs';
+import { throwError, Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import{SchoolAcademic, SchoolClass, SchoolSubject} from './student'
+import { SchoolAcademic, SchoolClass, SchoolSubject } from './student';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SchoolService {
-    uri:string="https://localhost:5000/SchoolMS/";
-  student:Array<SchoolAcademic>=[];
-  SchoolClaees:Array<SchoolClass>=[];
-  schoolId=1;
- constructor(private httpClient: HttpClient) {
-   }
-   httpOptions = {
+  uri: string = 'https://localhost:5000/SchoolMS/';
+  student: Array<SchoolAcademic> = [];
+  SchoolClaees: Array<SchoolClass> = [];
+  schoolId = 1;
+  constructor(private httpClient: HttpClient) {}
+  httpOptions = {
     headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
-  }
+      'Content-Type': 'application/json',
+    }),
+  };
 
-
-   get(): Observable<SchoolAcademic[]>{
-    console.log("get"+this.student);
+  get(): Observable<SchoolAcademic[]> {
+    console.log('get' + this.student);
     //return of(this.student);
 
-    return this.httpClient.get <SchoolAcademic[]>("https://localhost:5000/SchoolMS/api/SchoolAcademic/1",this.httpOptions)
-    .pipe(
-      catchError(this.errorHandler)
-    )
-
+    return this.httpClient
+      .get<SchoolAcademic[]>(
+        'https://localhost:5000/SchoolMS/api/SchoolAcademic/1',
+        this.httpOptions
+      )
+      .pipe(catchError(this.errorHandler));
   }
 
-  editAcademic(id):Observable<SchoolAcademic[]> {
+  editAcademic(id): Observable<SchoolAcademic[]> {
     debugger;
     return this.httpClient
-            .get<SchoolAcademic[]>(`${this.uri}/api/Academic/${id}`)
-            .pipe(
-              catchError(this.errorHandler)
-            )
-           
-    }
+      .get<SchoolAcademic[]>(`${this.uri}/api/Academic/${id}`)
+      .pipe(catchError(this.errorHandler));
+  }
 
-    saveAcademic(datas:any):Observable<SchoolAcademic[]>{
+  saveAcademic(datas: any): Observable<SchoolAcademic[]> {
+    const body = JSON.stringify(datas);
 
-      
-      const body=JSON.stringify(datas);
-
-      return this.httpClient.post<SchoolAcademic[]>("https://localhost:5000/SchoolMS/api/Academic"
-             ,body,this.httpOptions)
-             .pipe(
-              catchError(this.errorHandler)
-            )
-            
-        
-   
-    }
-    
-     
-    Getschoolcls():Observable<SchoolClass[]>{
-      debugger;
-      console.log("get"+this.SchoolClaees);
-      return this.httpClient.get<SchoolClass[]>("https://localhost:5000/SchoolMS/api/GetAllClass/1",this.httpOptions)
-      .pipe(
-        catchError(this.errorHandler)
+    return this.httpClient
+      .post<SchoolAcademic[]>(
+        'https://localhost:5000/SchoolMS/api/Academic',
+        body,
+        this.httpOptions
       )
-    }
+      .pipe(catchError(this.errorHandler));
+  }
 
-    SaveSchoolClass(datacls:any):Observable<SchoolClass[]>{
-      
-
-      
-      const body=JSON.stringify(datacls);
-
-      return this.httpClient.post<SchoolClass[]>("https://localhost:5000/SchoolMS/api/SaveSchoolClass"
-             ,body,this.httpOptions)
-             .pipe(
-              catchError(this.errorHandler)
-            )
-            
-    }
-
-    SaveSchoolSubject(DataSub:any):Observable<SchoolSubject[]>{
-
-      const body=JSON.stringify(DataSub);
-      return this.httpClient.post<SchoolSubject[]>("https://localhost:5000/SchoolMS/api/SaveSchoolSubject",body,this.httpOptions)
-      .pipe(
-        catchError(this.errorHandler)
+  Getschoolcls(): Observable<SchoolClass[]> {
+    debugger;
+    console.log('get' + this.SchoolClaees);
+    return this.httpClient
+      .get<SchoolClass[]>(
+        'https://localhost:5000/SchoolMS/api/GetAllClass/1',
+        this.httpOptions
       )
-    }
-     
+      .pipe(catchError(this.errorHandler));
+  }
 
-    GetSchoolSub():Observable<SchoolSubject[]>{
-      debugger;
-      console.log("get"+this.SchoolClaees);
-      return this.httpClient.get<SchoolSubject[]>("https://localhost:5000/SchoolMS/api/GetAllSubject/1",this.httpOptions)
-      .pipe(
-        catchError(this.errorHandler)
-      )
-    }
+  SaveSchoolClass(datacls: any): Observable<SchoolClass[]> {
+    const body = JSON.stringify(datacls);
 
-    GetSubCategory():Observable<SchoolSubject[]>{
-      debugger;
-      
-      return this.httpClient.get<SchoolSubject[]>("https://localhost:5000/SchoolMS/api/GetSubjectCategory/1",this.httpOptions)
-      .pipe(
-        catchError(this.errorHandler)
+    return this.httpClient
+      .post<SchoolClass[]>(
+        'https://localhost:5000/SchoolMS/api/SaveSchoolClass',
+        body,
+        this.httpOptions
       )
-    }
-    GetClassSubject(classId):Observable<SchoolClass[]>{
+      .pipe(catchError(this.errorHandler));
+  }
 
-      return this.httpClient.get<SchoolClass[]>("https://localhost:5000/SchoolMS/api/GetClassSubject/"+this.schoolId+"/"+classId+"",this.httpOptions)
-      .pipe(
-        catchError(this.errorHandler)
+  SaveSchoolSubject(DataSub: any): Observable<SchoolSubject[]> {
+    const body = JSON.stringify(DataSub);
+    return this.httpClient
+      .post<SchoolSubject[]>(
+        'https://localhost:5000/SchoolMS/api/SaveSchoolSubject',
+        body,
+        this.httpOptions
       )
-    }
+      .pipe(catchError(this.errorHandler));
+  }
+
+  GetSchoolSub(): Observable<SchoolSubject[]> {
+    debugger;
+    console.log('get' + this.SchoolClaees);
+    return this.httpClient
+      .get<SchoolSubject[]>(
+        'https://localhost:5000/SchoolMS/api/GetAllSubject/1',
+        this.httpOptions
+      )
+      .pipe(catchError(this.errorHandler));
+  }
+
+  GetSubCategory(): Observable<SchoolSubject[]> {
+    debugger;
+
+    return this.httpClient
+      .get<SchoolSubject[]>(
+        'https://localhost:5000/SchoolMS/api/GetSubjectCategory/1',
+        this.httpOptions
+      )
+      .pipe(catchError(this.errorHandler));
+  }
+  GetClassSubject(classId): Observable<SchoolClass[]> {
+    return this.httpClient
+      .get<SchoolClass[]>(
+        'https://localhost:5000/SchoolMS/api/GetClassSubject/' +
+          this.schoolId +
+          '/' +
+          classId +
+          '',
+        this.httpOptions
+      )
+      .pipe(catchError(this.errorHandler));
+  }
 
   errorHandler(error) {
     debugger;
     let errorMessage = '';
-    if(error.error instanceof ErrorEvent) {
+    if (error.error instanceof ErrorEvent) {
       // Get client-side error
       errorMessage = error.error.message;
     } else {
@@ -128,6 +134,5 @@ export class SchoolService {
     }
     console.log(errorMessage);
     return throwError(errorMessage);
- }
-  
+  }
 }
