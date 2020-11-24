3325bf973668d993926bce1e08941f2eb5895b76
gobal <45533772+satheeshkumarsoft@users.noreply.github.com>
2020-11-24 13:15:52 +0530

    11/23/

---
+++ src/app/master.ts
@@ -0,0 +1,39 @@
+export interface Religion {
+  ReligionID: number;
+  Religion: string;
+  Status: boolean;
+}
+export interface Nationality {
+  NationalityID: number;
+  Nationality: string;
+  Status: boolean;
+}
+export interface Disability {
+  DisabilityID: number;
+  Disability: string;
+  Status: boolean;
+}
+export interface Category {
+  CategoryID: number;
+  Category: string;
+  Status: boolean;
+}
+export interface Caste {
+  CasteID: number;
+  Caste: string;
+  Status: boolean;
+}
+export interface Country {
+  CountryID: number;
+  Country_Name: string;
+}
+export interface State {
+  StateID: number;
+  State_Name: string;
+  CountryID: number;
+}
+export interface City {
+  CityID: number;
+  City_Name: string;
+  StateID: number;
+}
--- src/app/student.ts
+++ src/app/student.ts
@@ -16,3 +16,41 @@ export interface SchoolSubject {
   SubjectNamee: string;
   Category_id: string;
 }
+export interface Student_Personal_Details {
+  Student_ID: bigint;
+  First_Name: string;
+  Middle_Name: string;
+  Last_Name: string;
+  Mother_Tongue: string;
+  Email_ID: string;
+  Date_Of_Join: Date;
+  Date_Of_Birth: Date;
+  Aadhar_Number: number;
+  Gender: boolean;
+  Is_Login_Details_Sent: boolean;
+}
+export interface Student_Class_Details {
+  Student_Class_ID: bigint;
+  Student_ID: bigint;
+  Student_Class_Section: string;
+}
+export interface Student_Parent_Details {
+  Student_Parent_ID: bigint;
+  Student_ID: bigint;
+  Mother_Name: string;
+  Father_Name: string;
+  Mother_Occupation: string;
+  Father_Occupation: string;
+  Moblie_Number: number;
+  Alternative_Moblie_number: number;
+  Email_ID: string;
+  Father_Aadhar_Number: number;
+  Is_Login_Details_Sent: boolean;
+}
+export interface Student_communcation_Address {
+  Student_Com_ID: bigint;
+  Student_ID: bigint;
+  House_Number: string;
+  Stread_Road: string;
+  Pin_Code: number;
+}
